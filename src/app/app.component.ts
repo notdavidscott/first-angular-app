433587c4073183f38ecb3530614876862e189a6d
David Scott Perez <david.scott.perez@live.com>
2018-07-04 22:37:46 -0700

    content updating
    updated some content and word choice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import { url } from 'inspector';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'David Scott Perez';
+  name = 'David Scott Perez';
+  title = 'Fullstack Javascript Dev'
   city = "Ladera Ranch";
   tagLine = '"Do it" - Emporer Palpatine';
   tagLine2 = '"Doin\'\ it!" - Josh Peck';
-  aboutMe = "Born and raised in Orange County, CA. Javascript Full Stack Web Deveveloper, Front-End Designer, User-Interface Creator, Family Man, Coffee Lover, Worship Leader @ Vineyard Laguna Niguel. I started my coding journey in 2017: half a year before enrolling in Bethel Tech, the first faith-based coding school! I have since aquired a variety of skills and have refined my ability to write functional and sleek code! Thank you for stopping by my portfolio."
+  aboutMe = "Born and raised in Orange County, CA. Javascript Full Stack Web Deveveloper, Front-End Designer, User-Interface Creator, Family Man, Coffee Lover, Worship Leader @ Vineyard Laguna Niguel. I started my coding journey in 2017: half a year before enrolling in Bethel Tech, the first faith-based coding school! I have since aquired a variety of skills and have refined my ability to write functional and sleek code! Thank you for stopping by my portfolio page."
   photo1 = 'url(http://notdavidscott.com/images/coffee2.0.jpg)';
 }
 
